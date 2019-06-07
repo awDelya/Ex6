bffb8c11491106b3f5d9a277f24b6f662803a299
Овчинникова Аделина <deley@mail.ru>
2019-06-08 03:03:54 +0500

    Add function vvod

--- Reqursiya/Reqursiya/Program.cs
+++ Reqursiya/Reqursiya/Program.cs
@@ -9,9 +9,21 @@ namespace Reqursiya
 {
     class Program
     {
+        private static int[] mas;
         static void Main()
         {
-
+            Color.Print("\n Задайте первое число последовательности а1: ");
+            int a1 = Number.Check(int.MinValue, int.MaxValue);
+            Color.Print("\n Задайте второе число последовательности а2: ");
+            int a2 = Number.Check(int.MinValue, int.MaxValue);
+            Color.Print("\n Задайте третье число последовательности а3: ");
+            int a3 = Number.Check(int.MinValue, int.MaxValue);
+            Color.Print("\n Задайте колличество чисел в последовательности N: ");
+            int N = Number.Check(int.MinValue, int.MaxValue);
+            mas = new int[N];
+            mas[0] = a1;
+            mas[1] = a2;
+            mas[2] = a3;
         }
     }
 }
