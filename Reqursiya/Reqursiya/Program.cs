dbeaf1ec6204569089adcf73c84242a0f0c5ea8a
Овчинникова Аделина <deley@mail.ru>
2019-06-08 16:40:08 +0500

    Add function FinfChislo

--- Reqursiya/Reqursiya/Program.cs
+++ Reqursiya/Reqursiya/Program.cs
@@ -9,21 +9,50 @@ namespace Reqursiya
 {
     class Program
     {
-        private static int[] mas;
+        private static int FindChislo(int N)
+        {
+            if(N==1)
+            {
+                return a1;
+            }
+            if(N==2)
+            {
+                return a2;
+            }
+            if(N==3)
+            {
+                return a3;
+            }
+            try
+            {
+                return (FindChislo(N - 1) + 1) / ((FindChislo(N - 2) + 2) * FindChislo(N - 3));
+            }
+            catch(DivideByZeroException)
+            {
+                Console.Clear();
+                Color.Print("\n По заданным параметрам числовую последовательность построить нельзя, попробуйте еще раз!", ConsoleColor.Red);
+                Main();
+                return 1;
+            }
+        }
+        private static int a1, a2, a3;
         static void Main()
         {
-            Color.Print("\n Задайте первое число последовательности а1: ");
-            int a1 = Number.Check(int.MinValue, int.MaxValue);
-            Color.Print("\n Задайте второе число последовательности а2: ");
-            int a2 = Number.Check(int.MinValue, int.MaxValue);
-            Color.Print("\n Задайте третье число последовательности а3: ");
-            int a3 = Number.Check(int.MinValue, int.MaxValue);
-            Color.Print("\n Задайте колличество чисел в последовательности N: ");
+            Color.Print("\n Задайте первое число последовательности а1: ", ConsoleColor.Yellow);
+            a1 = Number.Check(int.MinValue, int.MaxValue);
+            Color.Print("\n Задайте второе число последовательности а2: ", ConsoleColor.Yellow);
+            a2 = Number.Check(int.MinValue, int.MaxValue);
+            Color.Print("\n Задайте третье число последовательности а3: ", ConsoleColor.Yellow);
+            a3 = Number.Check(int.MinValue, int.MaxValue);
+            Color.Print("\n Задайте колличество чисел в последовательности N: ", ConsoleColor.Yellow);
             int N = Number.Check(int.MinValue, int.MaxValue);
-            mas = new int[N];
-            mas[0] = a1;
-            mas[1] = a2;
-            mas[2] = a3;
+            Color.Print("\n Ваша последовательность выглядит так: ", ConsoleColor.Yellow);
+            for(int i = N; i>0; i--)
+            {
+                Color.Print(Convert.ToString(FindChislo(i))+" ", ConsoleColor.Cyan);
+            }
+            Text.GoBackMenu();
+            Environment.Exit(0);
         }
     }
 }
