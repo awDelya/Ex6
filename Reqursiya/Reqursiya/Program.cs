2f4ede34ba723801976fed3b4b27f6e5385273bd
Овчинникова Аделина <deley@mail.ru>
2019-06-20 16:21:00 +0500

    Bugs fix

--- Reqursiya/Reqursiya/Program.cs
+++ Reqursiya/Reqursiya/Program.cs
@@ -11,52 +11,49 @@ namespace Reqursiya
     {
         private static double FindChislo(int N)
         {
-            if(N==1)
-            {
-                return a1;
-            }
-            if(N==2)
-            {
-                return a2;
-            }
-            if(N==3)
-            {
-                return a3;
-            }
-            try
-            {
-                return (FindChislo(N - 1) + 1) / (FindChislo(N - 2) + 2) * FindChislo(N - 3);
-            }
-            catch(DivideByZeroException)
+            if(N==1) return a1;
+            if(N==2) return a2;
+            if(N==3) return a3;
+            double a = FindChislo(N - 1) + 1;
+            double b = FindChislo(N - 2) + 2;
+            double c = FindChislo(N - 3);
+            if(b==0)
             {
                 Console.Clear();
-                Color.Print("\n По заданным параметрам числовую последовательность построить нельзя, попробуйте еще раз!", ConsoleColor.Red);
-                Main();
-                return 1;
+                Color.Print("\n По заданным параметрам числовую последовательность построить нельзя, попробуйте еще раз!\n", ConsoleColor.Red);
+                Main(); return 1;
             }
+            else
+                return a / b * c;
         }
+
         private static double a1, a2, a3;
         private static double[] mas;
-        static void Main()
+        private static int N;
+
+        private static void Create()
         {
             Color.Print("\n Задайте первое число последовательности а1: ", ConsoleColor.Yellow);
-            a1 = Number.Check(double.MinValue, double.MaxValue);
+            a1 = Number.Check(-100.0, 100.0);
             Color.Print("\n Задайте второе число последовательности а2: ", ConsoleColor.Yellow);
-            a2 = Number.Check(double.MinValue, double.MaxValue);
+            a2 = Number.Check(-100.0, 100.0);
             Color.Print("\n Задайте третье число последовательности а3: ", ConsoleColor.Yellow);
-            a3 = Number.Check(double.MinValue, double.MaxValue);
+            a3 = Number.Check(-100.0, 100.0);
             Color.Print("\n Задайте колличество чисел в последовательности N: ", ConsoleColor.Yellow);
-            int N = Number.Check(int.MinValue, int.MaxValue);
+            N = Number.Check(1, 10);
             mas = new double[N];
-            Color.Print("\n Ваша последовательность выглядит так: ", ConsoleColor.Yellow);
-            for(int i = N; i>0; i--)
+        }
+
+        private static void Rezult()
+        {
+            for (int i = N; i > 0; i--)
             {
-                mas[i-1] = FindChislo(i);
+                mas[i - 1] = FindChislo(i);
             }
             int m = 0;
             bool z = false;
             mas.Reverse();
-            for(int i = 0; i<N; i++)
+            for (int i = 0; i < N; i++)
             {
                 try
                 {
@@ -64,11 +61,11 @@ namespace Reqursiya
                     {
                         z = true;
                         m++;
-                    }  
+                    }
                     if (mas[i] < mas[i + 1])
                         m++;
                 }
-                catch(IndexOutOfRangeException) { }
+                catch (IndexOutOfRangeException) { }
                 Color.Print(Convert.ToString(mas[i]) + " ", ConsoleColor.Cyan);
             }
             if (m == N - 1 && !z)
@@ -77,8 +74,13 @@ namespace Reqursiya
                 Color.Print("\n\n Последовательность монтонно неубывающая.", ConsoleColor.Magenta);
             else
                 Color.Print("\n\n Последовательность не является строго возрастающей или монотонно неубывающей.", ConsoleColor.Magenta);
+        }
+        static void Main()
+        {
+            Create();
+            Color.Print("\n Ваша последовательность выглядит так: ", ConsoleColor.Yellow);
+            Rezult();
             Text.GoBackMenu();
-            Environment.Exit(0);
         }
     }
 }
